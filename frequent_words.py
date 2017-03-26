6221c925fc2284996718523cfa684e826b2e7ac8
Miquel Tolosa <miquel@monkingme.com>
2017-03-26 12:41:07 +0200

    feature vectors for weka

--- frequent_words.py
+++ frequent_words.py
@@ -7,6 +7,7 @@ import sys
 
 N = 10  # Default number of most frequent words
 MOST_FREQ = []
+F_VECTORS = []
 
 
 def readAllFiles():
@@ -48,11 +49,11 @@ def featureVectorOfText(text, words):
     return [(freqOfWordInText(text, w), w) for w in words]
 
 
-def printVectorAsWeka(feature_vect):
+def vectorAsWeka(feature_vect):
     weka_vect = ""
     for item in feature_vect:
-        weka_vect += str(item[0]) + " \"" + item[1] + "\", "
-    print("[" + weka_vect[:-2] + "]")
+        weka_vect += str(item[0]) + ", "
+    return "[" + weka_vect[:-2] + "]"
 
 
 def printMostFreq():
@@ -67,20 +68,31 @@ def calculateMostNFreq():
 
 
 def getAllFeatureVectors():
-    global MOST_FREQ
+    global MOST_FREQ, F_VECTORS
     mypath = "dataset/"
     files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     print('Reading ' + str(len(files)) + 'files...')
     for f in files:
         print('Feature vector for file ' + f + ': ')
         f_vector = featureVectorOfText(readFile(mypath + f), [w[0] for w in MOST_FREQ])
-        printVectorAsWeka(f_vector)
+        F_VECTORS.append(f_vector)
+        print(vectorAsWeka(f_vector))
+
+
+def saveFeatureVectorsToArrf():
+    new_path = 'feature_vectors.arff'
+    new_file = open(new_path, 'w')
+    text = ""
+    for v in F_VECTORS:
+        text += vectorAsWeka(v) + '\n'
+    new_file.write(text)
+    new_file.close()
 
 
 def printMenu():
     print('Welcome PNL - lab 2')
     print('Select what you want to do: ')
-    option = input('1. Calculate most N frequent words\n2. Calculate features vector\nor type "exit" to finish program execution\n')
+    option = input('1. Calculate most N frequent words\n2. Calculate features vector and save it to arrf file\nor type "exit" to finish program execution\n')
     if option == "1":
         global N
         N = int(input('Type the number of frequent word you want to calculate: '))
@@ -94,6 +106,7 @@ def printMenu():
         else:
             calculateMostNFreq()
             getAllFeatureVectors()
+            saveFeatureVectorsToArrf()
     elif option == "exit":
         sys.exit(0)
     else:
