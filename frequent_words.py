e62b4ab95012e950a32f0dffa5ada4219a7d17a2
Miquel Tolosa <miquel@monkingme.com>
2017-03-26 11:33:34 +0200

    feature vector working with count

--- frequent_words.py
+++ frequent_words.py
@@ -5,6 +5,7 @@ import re
 
 
 N = 10  # Default number of most frequent words
+MOST_FREQ = []
 
 
 def readAllFiles():
@@ -13,10 +14,16 @@ def readAllFiles():
     print('Reading ' + str(len(files)) + 'files...')
     text = ''
     for f in files:
-        file = open(mypath + f, 'r')
-        lines = file.readlines()
-        for l in lines:
-            text += l.strip("\n")
+        text += readFile(mypath + f)
+    return text
+
+
+def readFile(name):
+    text = ''
+    file = open(name, 'r')
+    lines = file.readlines()
+    for l in lines:
+        text += l.strip("\n")
     return text
 
 
@@ -27,14 +34,60 @@ def mostFrequentWordsOfText(n, text):
     return word_counts.most_common(n)
 
 
+def countOfWordInText(text, word):
+    count = 0
+    all_words = re.findall(r'\w+', text)
+    for w in all_words:
+        if w == word:
+            count = count + 1
+    return count
+
+
+def featureVectorOfText(text, words):
+    return [countOfWordInText(text, w) for w in words]
+
+
+def printMostFreq():
+    for i, w in enumerate(MOST_FREQ):
+        print(str(i) + ': ' + w[0] + '(' + str(w[1]) + ' times)')
+
+
+def calculateMostNFreq():
+    global MOST_FREQ
+    print('Most ' + str(N) + ' frequent words: ')
+    MOST_FREQ = mostFrequentWordsOfText(N, readAllFiles())
+
+
+def getAllFeatureVectors():
+    global MOST_FREQ
+    mypath = "dataset/"
+    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    print('Reading ' + str(len(files)) + 'files...')
+    for f in files:
+        print('Feature vector for file ' + f + ': ')
+        print(featureVectorOfText(readFile(mypath + f), [w[0] for w in MOST_FREQ]))
+
+
 def printMenu():
     print('Welcome PNL - lab 2')
-    global N
-    N = int(input('Type the number of frequent word you want to calculate: '))
-    print('Most ' + str(N) + ' frequent words: ')
-    f_words = mostFrequentWordsOfText(N, readAllFiles())
-    for i, w in enumerate(f_words):
-        print(str(i) + ': ' + w[0] + '(' + str(w[1]) + ' times)')
+    print('Select what you want to do: ')
+    option = int(input('1. Calculate most N frequent words\n2. Calculate features vector\n'))
+    if option == 1:
+        global N
+        N = int(input('Type the number of frequent word you want to calculate: '))
+        calculateMostNFreq()
+        printMostFreq()
+    elif option == 2:
+        print('Calculating feature vector with ' + str(N) + ' most frequent words')
+        global MOST_FREQ
+        if len(MOST_FREQ):
+            getAllFeatureVectors()
+        else:
+            calculateMostNFreq()
+            getAllFeatureVectors()
+    else:
+        print('You have selected invalid menu option')
+    printMenu()
 
 
 printMenu()
