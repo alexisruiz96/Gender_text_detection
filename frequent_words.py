4977bbee0113183b1b5785f1f079ca8d3c5e78ef
Miquel Tolosa <miquel@monkingme.com>
2017-03-26 22:44:06 +0200

    code comments

--- frequent_words.py
+++ frequent_words.py
@@ -12,6 +12,7 @@ F_VECTORS = []
 
 
 def readAllFiles():
+    """ Read all files from de /dataset directory and return it as a single string """
     mypath = "dataset/"
     files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     print('Reading ' + str(len(files)) + 'files...')
@@ -22,6 +23,7 @@ def readAllFiles():
 
 
 def readFile(name):
+    """ Read one file from path and return the text from file as a single string """
     text = ''
     file = open(name, 'r')
     lines = file.readlines()
@@ -31,6 +33,7 @@ def readFile(name):
 
 
 def mostFrequentWordsOfText(n, text):
+    """ Calculate most frequent n words from a given text """
     words = re.findall(r'\w+', text)
     low_words = [word.lower() for word in words]
     word_counts = Counter(low_words)
@@ -38,6 +41,7 @@ def mostFrequentWordsOfText(n, text):
 
 
 def freqOfWordInText(text, word):
+    """ Calculate the frecuency of given word in a given text """
     count = 0
     all_words = re.findall(r'\w+', text)
     for w in all_words:
@@ -47,28 +51,33 @@ def freqOfWordInText(text, word):
 
 
 def featureVectorOfText(text, words, gender):
+    """ Returns the feature vector as a tuple with the frecuencies and gender """
     return ([(freqOfWordInText(text, w), w) for w in words], gender)
 
 
 def vectorAsWeka(feature_vect):
+    """ Transform feature vector to weka format """
     weka_vect = ""
     for item in feature_vect:
         weka_vect += str(item[0]) + ", "
-    return "[" + weka_vect[:-2] + "]"
+    return weka_vect[:-2]
 
 
 def printMostFreq():
+    """ Print the stored most frequent words """
     for i, w in enumerate(MOST_FREQ):
-        print(str(i+1) + ': ' + w[0] + '(' + str(w[1]) + ' times)')
+        print(str(i + 1) + ': ' + w[0] + '(' + str(w[1]) + ' times)')
 
 
 def calculateMostNFreq():
+    """ Calculate the most n frequent from all files """
     global MOST_FREQ
     print('Most ' + str(N) + ' frequent words: ')
     MOST_FREQ = mostFrequentWordsOfText(N, readAllFiles())
 
 
 def getAllFeatureVectors():
+    """ Calculates feature vectors """
     global MOST_FREQ, F_VECTORS
     mypath = "dataset/"
     files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
@@ -81,23 +90,25 @@ def getAllFeatureVectors():
 
 
 def saveFeatureVectorsToArrf():
+    """ It saves the feature vectors in an .arff file """
     new_path = 'feature_vectors.arff'
-    os.remove(new_path)
+    os.remove(new_path)  # We crear content written before 
     new_file = open(new_path, 'w')
     text = "@RELATION p2_pln " + '\n\n\n'
     for x in range(N):
-        text += "@ATTRIBUTE" + "\t" + "number" + str(x + 1) + "\t" + "REAL" + '\n'
+        text += "@ATTRIBUTE" + "\t" + "number" + str(x + 1) + "\t" + "REAL" + '\n' # We need one attribute for each vector feature frequency value
     text += "@ATTRIBUTE gender {female,male}\n"
     text += '\n\n'
     text += "@DATA" + '\n'
 
     for v in F_VECTORS:
-        text += vectorAsWeka(v[0])[1:-1] + ',' + v[1] + '\n'
+        text += vectorAsWeka(v[0]) + ',' + v[1] + '\n'  # We write all the vectors in arff format
     new_file.write(text)
     new_file.close()
 
 
 def main():
+    """ Main function to print all options available to user or exit the program """
     print('Welcome PNL - lab 2')
     print('Select what you want to do: ')
     option = input('1. Calculate most N frequent words\n2. Calculate features vector and save it to arrf file\nor type "exit" to finish program execution\n')
