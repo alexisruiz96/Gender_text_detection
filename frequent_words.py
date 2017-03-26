108fbb306225881cc9fa9613d5f9dc134263b91b
Miquel Tolosa <miquel@monkingme.com>
2017-03-26 11:07:24 +0200

    most frequent N words working

--- frequent_words.py
+++ frequent_words.py
@@ -1,25 +1,40 @@
 from os import listdir
 from os.path import isfile, join
+from collections import Counter
+import re
 
-N = 10 # Default number of most frequent words
+
+N = 10  # Default number of most frequent words
+
+
+def readAllFiles():
+    mypath = "dataset/"
+    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    print('Reading ' + str(len(files)) + 'files...')
+    text = ''
+    for f in files:
+        file = open(mypath + f, 'r')
+        lines = file.readlines()
+        for l in lines:
+            text += l.strip("\n")
+    return text
+
+
+def mostFrequentWordsOfText(n, text):
+    words = re.findall(r'\w+', text)
+    low_words = [word.lower() for word in words]
+    word_counts = Counter(low_words)
+    return word_counts.most_common(n)
+
+
 def printMenu():
     print('Welcome PNL - lab 2')
     global N
     N = int(input('Type the number of frequent word you want to calculate: '))
+    print('Most ' + str(N) + ' frequent words: ')
+    f_words = mostFrequentWordsOfText(N, readAllFiles())
+    for i, w in enumerate(f_words):
+        print(str(i) + ': ' + w[0] + '(' + str(w[1]) + ' times)')
+
 
 printMenu()
-print(N)
-
-def readAllFiles():
-    mypath = "dataset/"
-    aux = 0
-    files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    print(len(files))
-    for f in files:
-        if aux < 3:
-            print(f)
-            aux = aux + 1
-        else:
-            break
-
-readAllFiles()
